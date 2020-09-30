65d4c9ee396a83cb7435cdb01d4b66bbd38601a9
Yoe <bravoy93@gmail.com>
2020-09-30 17:53:29 -0400

    Drawable Menu in XS view

--- src/Components/Layout/Layout.js
+++ src/Components/Layout/Layout.js
@@ -1,18 +1,39 @@
-import React from "react"
+import React, {useState} from "react"
+import ReactDOM from "react-dom"
 import SEO from "../seo"
 import Nav from "../Nav/Nav"
+import Backdrop from "../Backdrop/Backdrop";
+import Toolbar from "../Toolbar/Toolbar"
 
-const toTop = () => {
-  this.handleScrollToElement("nav");
-};
-
-const toolbarToggleClickHandler = () => {
-  // this.setState(prevState => {
-  //   return { toolbarOpen: !prevState.toolbarOpen };
-  console.log('toolbarToggleClickHandler clickeado')
-};
+// let toolbarOpen = false;
 
 export default function Layout({children}) {
+  const [toolbarOpen, setToolbarOpen] = useState(false);
+  const toTop = () => {
+    handleScrollToElement("nav");
+  };
+
+  function handleScrollToElement(elRef) {
+    const tesNode = ReactDOM.findDOMNode(this.refs[elRef]);
+    window.scrollTo({ top: tesNode.offsetTop, behavior: "smooth" });
+  }
+  
+  const toolbarToggleClickHandler = () => {
+    // this.setState(prevState => {
+    //   return { toolbarOpen: !prevState.toolbarOpen };
+    setToolbarOpen(!toolbarOpen)
+    console.log('toolbarToggleClickHandler clickeado')
+  };
+  
+  const backdropClickHandler = () => {
+    setToolbarOpen(false);
+  };
+
+  let backdrop;
+  if (toolbarOpen) {
+    backdrop = <Backdrop backdropClickHandler={backdropClickHandler} />;
+  }
+
   return (
     <>
       <SEO title="Unified Health Advisors"/>
@@ -22,6 +43,11 @@ export default function Layout({children}) {
         to_top={toTop}/>
       <div id="nav"/>
       {children}
+      {backdrop}
+      <Toolbar
+        show={toolbarOpen}
+        backdropClickHandler={backdropClickHandler}
+        to_top={toTop}/>
     </>
   )
 }
--- src/Components/Toolbar/Toolbar.js
+++ src/Components/Toolbar/Toolbar.js
@@ -1,47 +1,60 @@
 import React from "react";
 import { Link } from "gatsby";
 import styles from "./toolbar.module.css";
-// import "../Button/button.css";
+import {button} from "../Button/button.module.css";
 
-// import { fbPxTrigger } from "../..";
+import { fbPxTrigger } from "../../index";
 
-const ToolbarItem = props => (
-  <Link to={props.to} style={{ textDecoration: "none" }}>
-    <li className={`toolbar_item ${styles.toolbar_item}`} onClick={() => props.to_top()}>
+
+
+const ToolbarItem = props => (  
+  <li className={`toolbar_item ${styles.toolbar_item}`}>
+    <Link to={props.to} style={{ textDecoration: "none" }} onClick={() => props.to_top()} onKeyPress={() => props.to_top()}>
       {props.label}
-    </li>
-  </Link>
-)
+    </Link>
+  </li>)
 
 const toolbar = (props) => {
   let toolbarClasses = `toolbar ${styles.toolbar}`;
   if (props.show) {
     toolbarClasses = `toolbar ${styles.toolbar} open ${styles.open}`;
   }
+
+  const handleEnrollClick = () => {
+    fbPxTrigger();
+    props.to_top()
+  }
+  
   return (
     <nav className={toolbarClasses}>
-      <div className={`toolbar_container ${styles.toolbar_container}`} onClick={props.backdropClickHandler}>
+      <div className={`toolbar_container ${styles.toolbar_container}`} 
+      onClick={props.backdropClickHandler}
+      onKeyPress={props.backdropClickHandler}
+      role="button" 
+      aria-label="close menu"
+      tabIndex="-1">
         <div>
           <ul>
-            <div 
-              // onClick={fbPxTrigger}
-            >
-              <li className={`toolbar_item ${styles.toolbar_item}`} onClick={() => props.to_top()}>
-                Enroll
-              </li>
-            </div>
+            <li className={`toolbar_item ${styles.toolbar_item}`}>
+              <a href="/enroll" 
+              style={{ textDecoration: "none" }}
+              onClick={handleEnrollClick}
+              onKeyPress={handleEnrollClick}>
+               Enroll
+              </a> 
+            </li>
             <ToolbarItem to="/dental/" label="Dental" to_top={props.to_top} />
             <ToolbarItem to="/vision/" label="Vision / Hearing" to_top={props.to_top} />
             <ToolbarItem to="/about/" label="About" to_top={props.to_top} />
           </ul>
         </div>
-        <div className="call_toolbar">
+        <div className={`call_toolbar ${styles.call_toolbar}`}>
           <a
             data-replaceable-phone-dialable=""
-            className="`toolbar_item ${styles.toolbar_item}"
+            className={`toolbar_item ${styles.toolbar_item}`}
             href="tel:8559310267"
           >
-            <span className="button" id="`toolbar_button ${styles.toolbar_button">
+            <span className={`button ${button} ${styles.toolbar_button}`} id="toolbar_button">
               Call Now
             </span>
           </a>
