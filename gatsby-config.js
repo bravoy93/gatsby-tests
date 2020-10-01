914ba5ce5a2af2b77ad37be6357786b8585827b7
Yoe <bravoy93@gmail.com>
2020-10-01 11:40:09 -0400

    Enabled Gtag

--- gatsby-config.js
+++ gatsby-config.js
@@ -31,5 +31,17 @@ module.exports = {
         pixelId: 225246031877134,
       },
     },
+    /*Google Analitycs with gtag*/
+    {
+      resolve: `gatsby-plugin-gtag`,
+      options: {
+        // your google analytics tracking id
+        trackingId: `UA-144175240-1`,
+        // Puts tracking script in the head instead of the body
+        head: true,
+        // enable ip anonymization
+        anonymize: true,
+      }
+    },
   ],
 }
