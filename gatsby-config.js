326c122515bccb4379584484605ab62bf237aa18
Yoe <bravoy93@gmail.com>
2020-10-08 17:49:39 -0400

    [PWA] Maskable Icons in 192px and 512 px

--- gatsby-config.js
+++ gatsby-config.js
@@ -16,7 +16,6 @@ module.exports = {
   plugins: [
     `gatsby-plugin-react-helmet`,
     /*Manifest*/
-    'gatsby-plugin-offline',
     {
       resolve: `gatsby-plugin-manifest`,
       options: {
@@ -26,8 +25,63 @@ module.exports = {
         background_color: "#1f497d",
         theme_color: "#1f497d",
         display: "standalone",
-        icon: "src/images/icon.svg"
+        icon: "src/images/icon.svg",
+        icons: [
+          {
+            src: "icons/icon-48x48.png",
+            sizes: "48x48",
+            type: "image/png"
+          },
+          {
+            src: "icons/icon-72x72.png",
+            sizes: "72x72",
+            type: "image/png"
+          },
+          {
+            src: "icons/icon-96x96.png",
+            sizes: "96x96",
+            type: "image/png"
+          },
+          {
+            src: "icons/icon-144x144.png",
+            sizes: "144x144",
+            type: "image/png"
+          },
+          {
+            src: "icons/icon-192x192.png",
+            sizes: "192x192",
+            type: "image/png"
+          },
+          {
+            src: "icons/icon-256x256.png",
+            sizes: "256x256",
+            type: "image/png"
+          },
+          {
+            src: "icons/icon-384x384.png",
+            sizes: "384x384",
+            type: "image/png"
+          },
+          {
+            src: "icons/icon-512x512.png",
+            sizes: "512x512",
+            type: "image/png"
+          },
+          {
+            src: `/icons/icon-192x192-maskable.png`,
+            sizes: `192x192`,
+            type: `image/png`,
+            purpose: `maskable`
+          },
+          {
+            src: `/icons/icon-512x512-maskable.png`,
+            sizes: `512x512`,
+            type: `image/png`,
+            purpose: `maskable`
+          },
+        ]
       },
-    },    
+    },
+    'gatsby-plugin-offline'
   ],
 }
